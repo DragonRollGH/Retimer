702afd71bd4d8f8d9f8a1a22b8a55b77d8d31d4f
DragonRollGH <492782447@qq.com>
2022-01-11 17:02:10 +0800

    0111

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,15 +9,15 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     getMdl = new QFileSystemModel(this);
-    getMdl->setRootPath(QDir::currentPath());
-    ui->getTree->setModel(getMdl);
-    ui->getTree->setRootIndex(getMdl->index(QDir::currentPath()));
+//    getMdl->setRootPath(QDir::currentPath());
+//    ui->getTree->setModel(getMdl);
+//    ui->getTree->setRootIndex(getMdl->index(QDir::currentPath()));
     setMdl = new QFileSystemModel(this);
-    setMdl->setRootPath(QDir::currentPath());
-    ui->setTree->setModel(setMdl);
-    ui->setTree->setRootIndex(setMdl->index(QDir::currentPath()));
+//    setMdl->setRootPath(QDir::currentPath());
+//    ui->setTree->setModel(setMdl);
+//    ui->setTree->setRootIndex(setMdl->index(QDir::currentPath()));
 
-
+    this->setAcceptDrops(true);
 }
 
 MainWindow::~MainWindow()
@@ -26,13 +26,28 @@ MainWindow::~MainWindow()
 }
 
 
-void MainWindow::on_ConvertBtn_clicked()
+void MainWindow::dragEnterEvent(QDragEnterEvent *e)
 {
-    QModelIndex gI = ui->getTree->currentIndex();
-    QModelIndex sI = ui->setTree->currentIndex();
-    QString gF = getMdl->filePath(gI);
-    QString sF = setMdl->filePath(sI);
-    retime(gF,sF,RT_CFT);
+    e->acceptProposedAction();
+}
+
+void MainWindow::dropEvent(QDropEvent *e)
+{
+    QString dF = e->mimeData()->urls().first().toLocalFile();
+    if (e->pos().x() < this->width()/2)
+    {
+        getRP = dF;
+        getMdl->setRootPath(getRP);
+        ui->getTree->setModel(getMdl);
+        ui->getTree->setRootIndex(getMdl->index(getRP));
+    }
+    else
+    {
+        setRP = dF;
+        setMdl->setRootPath(setRP);
+        ui->setTree->setModel(setMdl);
+        ui->setTree->setRootIndex(setMdl->index(setRP));
+    }
 }
 
 void MainWindow::retime(QString g, QString s, int c, int m, int a)
@@ -41,55 +56,102 @@ void MainWindow::retime(QString g, QString s, int c, int m, int a)
     {
         return;
     }
-    HANDLE gHDL = CreateFileA((LPCSTR)g.toLocal8Bit(),GENERIC_READ,0,NULL,OPEN_EXISTING,0,0);
-    HANDLE sHDL = CreateFileA((LPCSTR)s.toLocal8Bit(),GENERIC_WRITE,0,NULL,OPEN_EXISTING,0,0);
+    HANDLE gHDL = CreateFileA((LPCSTR)g.toLocal8Bit(),\
+                              GENERIC_READ,0,NULL,OPEN_EXISTING,0,0);
+    HANDLE sHDL = CreateFileA((LPCSTR)s.toLocal8Bit(),\
+                              GENERIC_READ|GENERIC_WRITE,0,NULL,OPEN_EXISTING,0,0);
     if (gHDL == INVALID_HANDLE_VALUE || sHDL == INVALID_HANDLE_VALUE)
     {
         CloseHandle(gHDL);
         CloseHandle(sHDL);
         return;
     }
-    FILETIME cFT,mFT,aFT;
-    GetFileTime(gHDL,&cFT,&mFT,&aFT);
+    FILETIME gCT,gMT,gAT,sCT,sMT,sAT;
+    GetFileTime(gHDL,&gCT,&gAT,&gMT);
+    GetFileTime(sHDL,&sCT,&sAT,&sMT);
     switch (c) {
-    case RT_CFT:
-        SetFileTime(sHDL,&cFT,0,0);
+    case RT_GCT:
+        SetFileTime(sHDL,&gCT,0,0);
         break;
-    case RT_MFT:
-        SetFileTime(sHDL,&mFT,0,0);
+    case RT_GMT:
+        SetFileTime(sHDL,&gMT,0,0);
         break;
-    case RT_AFT:
-        SetFileTime(sHDL,&aFT,0,0);
+    case RT_GAT:
+        SetFileTime(sHDL,&gAT,0,0);
         break;
-    default:
+    case RT_SCT:
+        SetFileTime(sHDL,&sCT,0,0);
         break;
-    }
-    switch (m) {
-    case RT_CFT:
-        SetFileTime(sHDL,0,&cFT,0);
+    case RT_SMT:
+        SetFileTime(sHDL,&sMT,0,0);
         break;
-    case RT_MFT:
-        SetFileTime(sHDL,0,&mFT,0);
-        break;
-    case RT_AFT:
-        SetFileTime(sHDL,0,&aFT,0);
+    case RT_SAT:
+        SetFileTime(sHDL,&sAT,0,0);
         break;
     default:
         break;
     }
     switch (a) {
-    case RT_CFT:
-        SetFileTime(sHDL,0,0,&cFT);
+    case RT_GCT:
+        SetFileTime(sHDL,0,&gCT,0);
         break;
-    case RT_MFT:
-        SetFileTime(sHDL,0,0,&mFT);
+    case RT_GMT:
+        SetFileTime(sHDL,0,&gMT,0);
         break;
-    case RT_AFT:
-        SetFileTime(sHDL,0,0,&aFT);
+    case RT_GAT:
+        SetFileTime(sHDL,0,&gAT,0);
+        break;
+    case RT_SCT:
+        SetFileTime(sHDL,0,&sCT,0);
+        break;
+    case RT_SMT:
+        SetFileTime(sHDL,0,&sMT,0);
+        break;
+    case RT_SAT:
+        SetFileTime(sHDL,0,&sAT,0);
+        break;
+    default:
+        break;
+    }
+    switch (m) {
+    case RT_GCT:
+        SetFileTime(sHDL,0,0,&gCT);
+        break;
+    case RT_GMT:
+        SetFileTime(sHDL,0,0,&gMT);
+        break;
+    case RT_GAT:
+        SetFileTime(sHDL,0,0,&gAT);
+        break;
+    case RT_SCT:
+        SetFileTime(sHDL,0,0,&sCT);
+        break;
+    case RT_SMT:
+        SetFileTime(sHDL,0,0,&sMT);
+        break;
+    case RT_SAT:
+        SetFileTime(sHDL,0,0,&sAT);
         break;
     default:
         break;
     }
     CloseHandle(gHDL);
     CloseHandle(sHDL);
+}
+
+
+void MainWindow::on_ConvertBtn_clicked()
+{
+    QDirIterator sItr(setRP,QDir::Files|QDir::NoDotAndDotDot);
+    while(sItr.hasNext())
+    {
+        sItr.next();
+        QString sF = sItr.filePath();
+        QString gF = getRP+'/'+sItr.fileName();
+        if (QFile::exists(gF))
+        {
+            qDebug() << gF << sF;
+            retime(gF,sF,RT_GCT,RT_SCT);
+        }
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,9 @@
 #include <windows.h>
 #include <QMainWindow>
 #include <QFileSystemModel>
+#include <QDragEnterEvent>
+#include <QDropEvent>
+#include <QMimeData>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,6 +20,10 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void dragEnterEvent(QDragEnterEvent *e);
+    void dropEvent(QDropEvent *e);
+
 private slots:
     void on_ConvertBtn_clicked();
 
@@ -24,12 +31,17 @@ private:
     Ui::MainWindow *ui;
     QFileSystemModel *getMdl;
     QFileSystemModel *setMdl;
+    QString getRP;
+    QString setRP;
 
     enum RetimeType{
         RT_ORIGIN,
-        RT_CFT,
-        RT_MFT,
-        RT_AFT,
+        RT_GCT,
+        RT_GMT,
+        RT_GAT,
+        RT_SCT,
+        RT_SMT,
+        RT_SAT,
         RT_NOW
     };
     void retime(QString g,QString s,int c=0,int m=0,int a=0);
